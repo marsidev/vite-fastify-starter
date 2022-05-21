6a00fb0c5ea19fec80d4e4cf3a05b4d900153361
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-21 03:38:54 -0400

    build: new tsc build

--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -12,11 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const fastify_1 = __importDefault(require("fastify"));
 const static_1 = __importDefault(require("@fastify/static"));
 const path_1 = __importDefault(require("path"));
 const hello_1 = __importDefault(require("@controllers/hello"));
-const PORT = process.env.PORT || 3001;
+const { PORT = 3001, HOST = '0.0.0.0' } = process.env;
 const server = (0, fastify_1.default)();
 server.register(static_1.default, {
     root: path_1.default.join(__dirname, '../../../public')
@@ -24,7 +25,7 @@ server.register(static_1.default, {
 server.register(hello_1.default, { prefix: '/api/hello' });
 const start = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield server.listen(PORT);
+        yield server.listen(PORT, HOST);
         console.log(`Server listening on port ${PORT}`);
     }
     catch (err) {
