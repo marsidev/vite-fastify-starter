e54b2638453e06e4bdef23cf75f1cc86629aa80c
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-27 20:14:47 -0400

    docs: :memo: add documentation

--- packages/shared/src/utils/boolean.ts
+++ packages/shared/src/utils/boolean.ts
@@ -1,3 +1,11 @@
+/**
+ * It returns true if the value is truthy, and false if the value is falsy
+ * @param {unknown} a - unknown
+ */
 export const isTruthy = (a: unknown) => Boolean(a)
 
+/**
+ * If a is not null, then a is of type T.
+ * @param {T | null} a - T | null - a is either a T or null
+ */
 export const nonNull = <T>(a: T | null): a is T => a !== null
